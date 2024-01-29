902b7f3b2d034cda5ca11236347f9a9d7da17838
GrvExplorer <icodelife307@gmail.com>
2024-01-29 23:41:39 +0530

    overwrite

--- Instagram 2.0/src/_auth/forms/SignInForm.tsx
+++ Instagram 2.0/src/_auth/forms/SignInForm.tsx
@@ -1,11 +1,6 @@
 import { Button } from '@/components/ui/button'
-import { z } from "zod"
  
-
 function SignInForm() {
-  const formSchema = z.object({
-    username: z.string().min(2).max(50),
-  })
   
   return (
     <div>
